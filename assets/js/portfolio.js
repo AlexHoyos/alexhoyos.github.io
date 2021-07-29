27c20ff7e92f5ce7c18c63fcf789895c1813ca9f
Alex <alexhoyosf080302@gmail.com>
2021-07-29 16:46:37 -0700

    Portfolio with fullpagejs

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,21 +1,41 @@
-document.onload = function () {
 
+
+function activateNavItem(item){
+    item.addClass('btn-active').siblings().removeClass('btn-active');
+}
+
+function leaveSlide(origin, destination, direction){
+    activateNavItem($('#menu').find('a').eq(destination.index));
+}
+
+Vue.leaveSlide = function(origin, destination, direction) {
+    leaveSlide(origin, destination, direction);
 }
 
 var home = new Vue({
-    el: '#app',
+    el: '#portfolio',
     data (){
         return {
             lang: null,
-            projects: null
+            projects: null,
+            fullpage: {
+                autoScrolling: true,
+                anchors: ['home', 'about', 'projects', 'contact'],
+                onLeave: function(origin, destination, direction){
+                    Vue.leaveSlide(origin, destination, direction)
+                }
+            }
         }
     },
     mounted() {
+        
         axios
             .get('./assets/lang/en-US.json')
             .then(response => {
                 this.lang = response.data
                 this.projects = response.data.projects.projects
+                
             })
-    }
+
+    },
 })
